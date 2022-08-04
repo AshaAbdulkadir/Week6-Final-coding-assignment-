957f034a106c6e2ed195a3436e5dce93702b1a98
AshaAbdu <halge87@yahoo.com>
2022-08-03 23:11:16 -0500

    3rd commit

--- src/week6JavaFinalCodingProject/Deck.java
+++ src/week6JavaFinalCodingProject/Deck.java
@@ -1,5 +1,22 @@
 package week6JavaFinalCodingProject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+	
+	List<Card> cards =new ArrayList<Card>();
+	
+	
+	public void Shuffle() {
+		
+		
+	}
+	
+	public void Draw() {
+		
+		
+	}
+	
 
 }
